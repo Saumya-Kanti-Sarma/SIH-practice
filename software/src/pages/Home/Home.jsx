639c08890035932e5f4c7b3b9260ebc897a29556
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-08-27 00:35:16 +0530

    chores: afjusted few tweeks

--- software/src/pages/Home/Home.jsx
+++ software/src/pages/Home/Home.jsx
@@ -1,17 +1,16 @@
 import Room from "./components/Room/Room.jsx";
-import Sidebar from "./components/Sidebar/Sidebar";
 import "./Home.css";
 
 const Home = () => {
   return (
     <main className="home">
-      <Sidebar />
+
       <div className="home-content-area">
         <Room roomNo={101} status="ready" />
         <Room roomNo={701} status="empty" />
         <Room roomNo={155} status="cleaning" />
         <Room roomNo={211} status="inspection" />
-        <Room roomNo={432} status="Maintainance" />
+        <Room roomNo={432} status="maintainance" />
         <Room roomNo={1005} status="booked" />
       </div>
     </main>
